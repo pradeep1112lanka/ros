f1e93e70293ee1a30f09660eb9b54aae1c039305
pradeep1112lanka <129276941+pradeep1112lanka@users.noreply.github.com>
2023-07-24 11:50:39 +0530

    draw_circle.py

--- draw_circle.py
+++ draw_circle.py
@@ -3,17 +3,17 @@ import rospy
 from geometry_msgs.msg import Twist
 
 if __name__=='__main__':
-    rospy.init_node("draw_circle")
-    rospy.loginfo("this node has been started")
+    rospy.init_node("draw_circle")  #used to initate the node with custom name which is independent of the file name
+    rospy.loginfo("this node has been started")  #loginfo to show the msg on the terminal
 
-    pub = rospy.Publisher("/turtle1/cmd_vel" , Twist , queue_size=10)
+    pub = rospy.Publisher("/turtle1/cmd_vel" , Twist , queue_size=10)  #defining the publisher
 
-    rate = rospy.Rate(1.0)
+    rate = rospy.Rate(1.0) #refresh rate @10hz
 
     while not rospy.is_shutdown():
         msg = Twist()
         msg.linear.x=5
         msg.linear.y =1
         msg.angular.z=3
-        pub.publish(msg)
+        pub.publish(msg)   #publishing the topic msg
         rate.sleep()
