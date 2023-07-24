ad61b64c28bebe3270c2fd3de96dc49094a5e470
pradeep1112lanka <129276941+pradeep1112lanka@users.noreply.github.com>
2023-07-24 11:54:05 +0530

    pose_subscriber.py

--- pose_subscriber.py
+++ pose_subscriber.py
@@ -3,11 +3,11 @@ import rospy
 from turtlesim.msg import Pose 
 
 def pose_callback(msg: Pose):
-    rospy.loginfo("(" +str(msg.x) + "," +str(msg.y)+ ")")
+    rospy.loginfo("(" +str(msg.x) + "," +str(msg.y)+ ")")   #writing the loginfo in another format
     rospy.sleep(0.5)
 if __name__ == '__main__':
     rospy.init_node("turtle_pose_subscriber")
-    sub = rospy.Subscriber("/turtle1/pose", Pose , callback=pose_callback)    
+    sub = rospy.Subscriber("/turtle1/pose", Pose , callback=pose_callback)   #defining the subscriber node  
     rospy.loginfo("Node has been started")
 
     rospy.spin()  #Blocks until ROS node is shutdown. Yields activity to other threads
