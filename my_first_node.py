537c9a6de77042a399375e3606a45cadf1fb66bb
pradeep1112lanka <129276941+pradeep1112lanka@users.noreply.github.com>
2023-07-24 11:53:03 +0530

    my_first_node.py

--- my_first_node.py
+++ my_first_node.py
@@ -3,13 +3,13 @@ import rospy
 
 if __name__ == '__main__':
     rospy.init_node("test_node")  #name of the node running in ros master
-    rospy.loginfo("hello there")
-    rospy.logerr("this is a warning")
-    rospy.logwarn("this is an warning")
+    rospy.loginfo("hello there")  #msg info
+    rospy.logerr("this is a warning") #msg error
+    rospy.logwarn("this is an warning")  #msg warning
 
-    rospy.sleep(0.5)
+    rospy.sleep(0.5)    #sleep for 0.5 sec 
     rate = rospy.Rate(1.0)
-    while not rospy.is_shutdown():
+    while not rospy.is_shutdown():   #this runs untill the rospy file is not stopped 
         rospy.loginfo("this is the first node written by pradeep")
-        rate.sleep()
-    rospy.loginfo("end of the program")
+        rate.sleep()  #sleep @rate
+    rospy.loginfo("end of the program")  #end note after closing program
